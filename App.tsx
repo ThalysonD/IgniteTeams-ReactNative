6a425b082b4a3c871079c8afdf394296fd6166e7
ThalysonD <thalysondornelasgomes14@gmail.com>
2023-11-15 18:31:49 -0300

    Added Design System & Style Guide and Main Themes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import theme from './src/theme'
 import { Groups } from "@screens/Groups/intex";
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Groups/>
+    </ThemeProvider>
   );
 }
 
